77e08ebc4a235e12dbf53e2e1d0ba2df603d907a
Rajdeep Bhattacharya <161099290+localgroup@users.noreply.github.com>
2024-08-07 13:20:59 +0530

    Initialize the project

---
+++ frontend/src/components/Form.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import api from "../api"
+import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants";
+import "../styles/Form.css"
+
+
+export default function Form({route, method}) {
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const [loading, setLoading] = useState(false)
+    const navigate = useNavigate()
+
+    const handleSubmit = async (e) => {
+        setLoading(true);
+        e.preventDefault();
+
+        try {
+            const res = await api.post(route, { username, password })
+            if (method === "login") {
+                localStorage.setItem(ACCESS_TOKEN, res.data.access);
+                localStorage.setItem(REFRESH_TOKEN, res.data.refresh);
+                navigate("/")
+            } else {
+                navigate("/login")
+            }
+        } catch (error) {
+            alert(error)
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    const head = method === "login" ? "Login" : "Register"
+
+    return (
+        <form onSubmit={handleSubmit} className="form-container">
+            <h1>{head}</h1>
+            <input 
+                type="text"
+                value={username}
+                className="form-input"
+                placeholder="UserName"
+                onChange={(e) => setUsername(e.target.value)}
+            />
+            <input 
+                type="password"
+                value={password}
+                className="form-input"
+                placeholder="Password"
+                onChange={(e) => setPassword(e.target.value)}
+            />
+            <button className="form-button" type="submit">
+                {head}
+            </button>
+        </form>
+    )
+}
---
+++ frontend/src/pages/NotFound.jsx
@@ -0,0 +1,12 @@
+export default function NotFound() {
+    return (
+        <div>
+            <center>
+                <h1>404</h1>
+                <p>
+                    The page you are looking for doesn't exists...
+                </p>
+            </center>
+        </div>
+    )
+}
